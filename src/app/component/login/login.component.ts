1b3b33f9ac134dab7d4fbf92a130aaaa6b719f5a
tovariddt <domingueztovarisraeldavid99@gmail.com>
2024-07-16 21:45:21 -0600

    Alerta Sencilla
    Aun puede mejorar

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
 
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -14,44 +15,42 @@ import { Router } from '@angular/router';
 export class LoginComponent {
 
   loginObj: Login;
-errorMessageEmail: any;
-errorMessagePassword: any;
 
   constructor(private router: Router) {
     this.loginObj = new Login();
   }
 
-
   onLogin() {
-    
-      // Validar el formato del correo electrónico
-      if (this.loginObj.Email !== "Correo@gmail.com" ) {
-        alert("Por favor, ingresa un correo electrónico válido.");
-        return;
-      }
-    
-      // Validar la contraseña
-      if (this.loginObj.Password !== "1234") {
-        alert("Credenciales incorrectas. Verifica la contraseña.");
-        return;
-      }
-    
-      // Mostrar una alerta con la información escrita
-      const mensajeAlerta = "¡Bienvenido! Has iniciado sesión correctamente."+
-      "Tu Correo es "+this.loginObj.Email+" Tu Contraseña es "+this.loginObj.Password;
-      alert(mensajeAlerta);
-    
-      // Redirigir al usuario a la ruta "/home"
-      this.router.navigateByUrl('/home');
-    }
-    
 
+ 
+    // Validar el formato del correo electrónico
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    if (this.loginObj.Email !== 'Correo@gmail.com' || !emailRegex.test(this.loginObj.Email)) {
+      // Mostrar la alerta
+      document.getElementById('AlertEmail')!.classList.remove('d-none');
+      return;
+    }
+    // Validar la contraseña
+    if (this.loginObj.Password !== "1234") {
+      document.getElementById('AlertPaswork')!.classList.remove('d-none');
+        return;
+    }
+
+    // Mostrar una alerta con la información escrita
+    const mensajeAlerta = "¡Bienvenido! Has iniciado sesión correctamente."+
+    "Tu Correo es "+this.loginObj.Email+" Tu Contraseña es "+this.loginObj.Password;
+    alert(mensajeAlerta);
+    // Redirigir al usuario a la ruta "/home"
+    this.router.navigateByUrl('/home');
+}
 
 onRegister() {
   this.router.navigateByUrl('/register');
 }
 
 }
+
+
 
 export class Login {
   Email: string;
